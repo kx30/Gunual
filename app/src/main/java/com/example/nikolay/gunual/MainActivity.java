bd8d7bd1c08cbcfbb5215fe1d9d779b7a5d82dae
kx30 <nikolay199808@gmail.com>
2018-12-17 15:00:46 +0300

    Add styles, colors and menu

--- app/src/main/java/com/example/nikolay/gunual/MainActivity.java
+++ app/src/main/java/com/example/nikolay/gunual/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.nikolay.gunual;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                Toast.makeText(this, "favorite", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_compare:
+                Toast.makeText(this, "to compare", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_settings:
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_about_us:
+                Toast.makeText(this, "about us", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+        Log.d(TAG, "onCreate: started.");
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
+        Log.d(TAG, "initToolbar: initialized.");
+    }
+
 }
