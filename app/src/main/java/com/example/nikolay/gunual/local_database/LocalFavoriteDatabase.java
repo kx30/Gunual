3e941beaea9b39cfc469958d47048a1dcaf1a91d
kx30 <nikolay199808@gmail.com>
2019-04-09 18:23:43 +0300

    Backuo

--- app/src/main/java/com/example/nikolay/gunual/local_database/LocalFavoriteDatabase.java
+++ app/src/main/java/com/example/nikolay/gunual/local_database/LocalFavoriteDatabase.java
@@ -7,12 +7,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class LocalFavoriteDatabase extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Favorites.db";
-    public static final String TABLE_NAME = "favorites";
-    public static final String COL_1 = "TITLE";
-    public static final String COL_2 = "COUNTRY";
-    public static final String COL_3 = "TYPE_OF_BULLET";
-    public static final String COL_4 = "YEAR_OF_PRODUCTION";
+    private static final String DATABASE_NAME = "Favorites.db";
+    private static final String TABLE_NAME = "favorites";
+    private static final String COL_1 = "ID";
+    private static final String COL_2 = "TITLE";
+    private static final String COL_3 = "COUNTRY";
+    private static final String COL_4 = "YEAR_OF_PRODUCTION";
+    private static final String COL_5 = "TYPE_OF_BULLET";
+    private static final String COL_6 = "EFFECTIVE_RANGE";
+    private static final String COL_7 = "MUZZLE_VELOCITY";
+    private static final String COL_8 = "LENGTH";
+    private static final String COL_9 = "BARREL_LENGTH";
+    private static final String COL_10 = "WEIGHT";
+    private static final String COL_11 = "FEED_SYSTEM";
+    private static final String COL_12 = "DESCRIPTION";
+    private static final String COL_13 = "IMAGE_URL";
 
     public LocalFavoriteDatabase(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -21,7 +30,10 @@ public class LocalFavoriteDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " + TABLE_NAME + " (TITLE text, COUNTRY text, TYPE_OF_BULLET text, YEAR_OF_PRODUCTION text)");
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME +
+                " (ID integer primary key autoincrement, TITLE text, COUNTRY text, YEAR_OF_PRODUCTION text, TYPE_OF_BULLET text," +
+                " EFFECTIVE_RANGE text, MUZZLE_VELOCITY text, LENGTH text, BARREL_LENGTH text, " +
+                "WEIGHT text, FEED_SYSTEM text, DESCRIPTION text, IMAGE_URL text)");
     }
 
     @Override
@@ -30,13 +42,23 @@ public class LocalFavoriteDatabase extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    public boolean insertData(String title, String country, String typeOfBullet, String yearOfProduction) {
+    public boolean insertData(String title, String country, String typeOfBullet, String yearOfProduction, String effectiveRange,
+                              String muzzleVelocity, String length, String barrelLength, String weight, String feedSystem,
+                              String description, String imageUrl) {
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1, title);
-        contentValues.put(COL_2, country);
-        contentValues.put(COL_3, typeOfBullet);
+        contentValues.put(COL_2, title);
+        contentValues.put(COL_3, country);
         contentValues.put(COL_4, yearOfProduction);
+        contentValues.put(COL_5, typeOfBullet);
+        contentValues.put(COL_6, effectiveRange);
+        contentValues.put(COL_7, muzzleVelocity);
+        contentValues.put(COL_8, length);
+        contentValues.put(COL_9, barrelLength);
+        contentValues.put(COL_10, weight);
+        contentValues.put(COL_11, feedSystem);
+        contentValues.put(COL_12, description);
+        contentValues.put(COL_13, imageUrl);
         long result = database.insert(TABLE_NAME, null, contentValues);
         return result != -1;
     }
